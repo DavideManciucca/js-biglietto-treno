d21f3d65f73b664e6077e6cd73b5d72e3e599bfa
DavideManciucca <davidemanciucca@gmail.com>
2022-04-06 16:33:37 +0200

    esercizio funzionante, calcola lo sconto correttamente

---
+++ js/script.js
@@ -0,0 +1,23 @@
+let agePass = parseInt(prompt("inserisci quì la tua età"))
+let travelLength = parseInt(prompt("inserisci quì la lunghezza della tratta"))
+const ticketFee = 0.21
+
+let ticketPrice = ticketFee * travelLength;
+console.log("prezzo base",ticketPrice);
+let finalCost
+
+
+if(agePass < 18){
+  finalDiscount = ticketPrice * (20 /100);
+} 
+if(agePass >= 65){
+  finalDiscount = ticketPrice * (40/100);
+}
+console.log("Lo sconto da applicare all'importo è di", finalDiscount);
+
+finalCost = ticketPrice - finalDiscount
+console.log("Il prezzo finale del biglietto viene", finalCost);
+
+
+
+
